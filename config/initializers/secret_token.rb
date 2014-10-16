a1d4f027053a6fbca4ad4459427888a60f6633c8
gonzalojpv <gonzalojpv@openmailbox.org>
2014-10-16 15:26:51 -0500

    GP, init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BodyMass::Application.config.secret_key_base = '55132af7f84fc8273155afbd5d6b704db8c89283345dec53f1cfad3cd4fb90f284248018c05ee327cec459b96fa434000ba837745438d23786f00f0bdee623ba'
