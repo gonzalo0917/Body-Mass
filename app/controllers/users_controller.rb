e8d5108f65caeea4186bb94ba5e1d8c28a42a54b
gonzalojpv <gonzalojpv@openmailbox.org>
2014-10-16 16:14:54 -0500

    GP, redirect to mass index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,8 @@ class UsersController < ApplicationController
       if @user.save
         session[:user] = @user
          
-        format.html { redirect_to @user, notice: 'User was successfully created.', :controller => 'mass', :action => "index" }
-        #format.json { render action: 'index', status: :created, location: @user }
+        format.html { redirect_to notice: 'User was successfully created.',controller:'mass' }
+        format.json { render action: 'index', status: :created, location: @user }
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
